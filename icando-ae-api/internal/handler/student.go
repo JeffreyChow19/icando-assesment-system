fa408c2fde451152097ed8359f8f81495cb715cd
chaerla <chelgchen@gmail.com>
2024-03-05 23:21:59 +0700

    feat: student handler

---
+++ icando-ae-api/internal/handler/student.go
@@ -0,0 +1,52 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
+	"github.com/pkg/errors"
+	"icando/internal/domain/service"
+	"icando/internal/model/dto"
+	"icando/utils/httperror"
+	"icando/utils/response"
+	"net/http"
+)
+
+type StudentHandler interface {
+	Post(c *gin.Context)
+}
+
+type StudentHandlerImpl struct {
+	studentService service.StudentService
+}
+
+func NewStudentHandlerImpl(studentService service.StudentService) *StudentHandlerImpl {
+	return &StudentHandlerImpl{
+		studentService: studentService,
+	}
+}
+
+func (h *StudentHandlerImpl) Post(c *gin.Context) {
+	var payload dto.CreateStudentDto
+
+	if err := c.ShouldBindJSON(&payload); err != nil {
+		var ve validator.ValidationErrors
+		if errors.As(err, &ve) {
+			out := make([]httperror.FieldError, len(ve))
+			for i, fe := range ve {
+				out[i] = httperror.FieldError{Field: fe.Field(), Message: httperror.MsgForTag(fe.Tag()), Tag: fe.Tag()}
+			}
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"errors": out})
+			return
+		}
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{"errors": "Invalid body"})
+		return
+	}
+	class, err := h.studentService.AddStudent(uuid.New(), payload)
+	if err != nil {
+		c.AbortWithStatusJSON(err.StatusCode, gin.H{"errors": err.Err.Error()})
+		return
+	}
+	createdMsg := "Created"
+	c.JSON(http.StatusCreated, response.NewBaseResponse(&createdMsg, *class))
+}
