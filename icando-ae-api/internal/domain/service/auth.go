422a6f96285c9f462bb2c6dc978a8c916d178914
Alexander Jason <alexanderjason526@gmail.com>
2024-03-05 16:43:01 +0700

    feat: add auth service

---
+++ icando-ae-api/internal/domain/service/auth.go
@@ -0,0 +1,122 @@
+package service
+
+import (
+	"github.com/golang-jwt/jwt"
+	"github.com/google/uuid"
+	"github.com/pkg/errors"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+	"icando/internal/domain/repository"
+	"icando/internal/model"
+	"icando/internal/model/dao"
+	"icando/internal/model/dto"
+	"icando/lib"
+	"icando/utils/httperror"
+	"net/http"
+	"time"
+)
+
+type AuthService interface {
+	Login(loginDro dto.LoginDto) (*dao.AuthDao, error)
+	ChangePassword(id uuid.UUID, dto dto.ChangePasswordDto) *httperror.HttpError
+}
+
+type AuthServiceImpl struct {
+	learningDesignerRepository repository.LearningDesignerRepository
+	config                     *lib.Config
+}
+
+func NewAuthServiceImpl(learningDesignerRepository repository.LearningDesignerRepository, config *lib.Config) *AuthServiceImpl {
+	return &AuthServiceImpl{
+		learningDesignerRepository: learningDesignerRepository,
+		config:                     config,
+	}
+}
+
+func (s *AuthServiceImpl) Login(loginDto dto.LoginDto) (*dao.AuthDao, error) {
+	user, err := s.learningDesignerRepository.FindUserByEmail(loginDto.Email)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, UserNotFoundErr
+		}
+		return nil, httperror.InternalServerError
+	}
+
+	if !s.checkPassword(loginDto.Password, user.Password) {
+		return nil, httperror.UnauthorizedError
+	}
+
+	authDao, err := s.buildAuthDao(user)
+	if err != nil {
+		return nil, httperror.InternalServerError
+	}
+
+	return authDao, nil
+}
+
+func (s *AuthServiceImpl) ChangePassword(id uuid.UUID, dto dto.ChangePasswordDto) *httperror.HttpError {
+	user, err := s.learningDesignerRepository.FindUserById(id)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return UserNotFoundErr
+		}
+		return httperror.InternalServerError
+	}
+
+	if !s.checkPassword(dto.OldPassword, user.Password) {
+		return httperror.UnauthorizedError
+	}
+
+	user.Password = s.hashPassword(dto.NewPassword)
+	err = s.learningDesignerRepository.UpdateUserInfo(user)
+	if err != nil {
+		return httperror.InternalServerError
+	}
+	return nil
+}
+
+func (s *AuthServiceImpl) buildAuthDao(user *model.LearningDesiner) (*dao.AuthDao, error) {
+	token, err := s.generateToken(user)
+	if err != nil {
+		return nil, err
+	}
+	authDao := dao.AuthDao{
+		User: dao.LearningDesignerDao{
+			ID:        user.ID,
+			FirstName: user.FirstName,
+			LastName:  user.LastName,
+			Email:     user.Email,
+			// Role:        "Learning Designer",
+		},
+		Token: token,
+	}
+
+	return &authDao, nil
+}
+
+
+func (s *AuthServiceImpl) generateToken(user *model.LearningDesiner) (string, error) {
+	token := jwt.NewWithClaims(
+		jwt.SigningMethodHS256, jwt.MapClaims{
+			"id":  user.ID,
+			"exp": time.Now().Add(time.Hour * 8).Unix(),
+		},
+	)
+	tokenString, err := token.SignedString([]byte(s.config.JwtSecret))
+	return tokenString, err
+}
+
+func (s *AuthServiceImpl) hashPassword(password string) string {
+	bytes, _ := bcrypt.GenerateFromPassword([]byte(password), 10)
+	return string(bytes)
+}
+
+func (s *AuthServiceImpl) checkPassword(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+var UserNotFoundErr = &httperror.HttpError{
+	StatusCode: http.StatusNotFound,
+	Err:        errors.New("User not found"),
+}
--- icando-ae-api/internal/domain/service/service.go
+++ icando-ae-api/internal/domain/service/service.go
@@ -5,6 +5,6 @@ import "go.uber.org/fx"
 var Module = fx.Module(
 	"service",
 	fx.Options(
-		fx.Provide(),
+		fx.Provide(fx.Annotate(NewAuthServiceImpl, fx.As(new(AuthService)))),
 	),
 )
