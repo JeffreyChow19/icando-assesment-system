c964dd472b8b4fa946b6ed09cb3527402c3a175c
Alexander Jason <alexanderjason526@gmail.com>
2024-03-06 04:25:27 +0700

    feat: add protected route

--- icando-ae-api/internal/route/auth.go
+++ icando-ae-api/internal/route/auth.go
@@ -2,21 +2,22 @@ package route
 
 import (
 	"icando/internal/handler"
-	// "icando/internal/middleware"
+	"icando/internal/middleware"
 	// "icando/internal/model"
 	"github.com/gin-gonic/gin"
 )
 
-type AuthRoute struct{
-	authHandler handler.AuthHandler
+type AuthRoute struct {
+	authHandler    handler.AuthHandler
+	authMiddleware middleware.AuthMiddleware
 }
 
-func (r AuthRoute) Setup(engine *gin.RouterGroup){
-	group:=engine.Group("/auth")
-	group.POST("/login", r.authHandler.Login)
+func (r AuthRoute) Setup(engine *gin.RouterGroup) {
+	group := engine.Group("/auth")
+	group.POST("/login", r.authMiddleware.Handler("Learning Designer"), r.authHandler.Login)
 }
 
-func NewAuthRoute(authHandler handler.AuthHandler) *AuthRoute{
+func NewAuthRoute(authHandler handler.AuthHandler) *AuthRoute {
 	return &AuthRoute{
 		authHandler: authHandler,
 	}
