dc4f6867afa6108f9590d9547355e784bd787699
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2024-03-08 09:03:49 +0700

    fix auth middleware

--- icando-ae-api/internal/middleware/auth.go
+++ icando-ae-api/internal/middleware/auth.go
@@ -2,7 +2,10 @@ package middleware
 
 import (
 	"fmt"
-	"icando/internal/domain/service"
+	"icando/internal/domain/repository"
+	"icando/internal/model"
+	"icando/internal/model/dao"
+	"icando/internal/model/dto"
 	"icando/lib"
 	"net/http"
 	"strings"
@@ -15,35 +18,68 @@ import (
 )
 
 type AuthMiddleware struct {
-	service service.LearningDesignerService
-	config  *lib.Config
+	studentRepository          repository.StudentRepository
+	teacherRepository          repository.TeacherRepository
+	learningDesignerRepository repository.LearningDesignerRepository
+	config                     *lib.Config
 }
 
-func NewAuthMiddleware(service service.LearningDesignerService, config *lib.Config) *AuthMiddleware {
+func NewAuthMiddleware(
+	config *lib.Config,
+	studentRepository repository.StudentRepository,
+	teacherRepository repository.TeacherRepository,
+	learningDesignerRepository repository.LearningDesignerRepository,
+) *AuthMiddleware {
 	return &AuthMiddleware{
-		service: service,
-		config:  config,
+		studentRepository:          studentRepository,
+		teacherRepository:          teacherRepository,
+		learningDesignerRepository: learningDesignerRepository,
+		config:                     config,
 	}
 }
 
-func (m *AuthMiddleware) Handler(role string) gin.HandlerFunc {
+func (m *AuthMiddleware) Handler(role model.Role) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		authHeader := c.Request.Header.Get("Authorization")
+
 		t := strings.Split(authHeader, " ")
+
 		if len(t) == 2 && t[0] == "Bearer" {
 			authToken := t[1]
 			authorized, err := m.authorize(authToken)
+
 			if authorized == nil {
 				c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
 				return
 			}
-			user, httpError := m.service.FindUserById(*authorized)
-			if httpError != nil {
-				c.AbortWithStatusJSON(httpError.StatusCode, gin.H{"error": httpError.Err.Error()})
-				return
+
+			if authorized.Role == model.ROLE_TEACHER {
+				teacher, err := m.teacherRepository.GetTeacher(dto.GetTeacherFilter{ID: &authorized.ID})
+
+				c.Set("InstitutionID", teacher.InstitutionID)
+
+				if err != nil {
+					c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": errors.New("Teacher not found")})
+					return
+				}
+			} else if authorized.Role == model.ROLE_STUDENT {
+				_, err := m.studentRepository.GetOne(dto.GetStudentFilter{ID: &authorized.ID})
+
+				if err != nil {
+					c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": errors.New("Student not found")})
+					return
+				}
+			} else if authorized.Role == model.ROLE_LEARNING_DESIGNER {
+				_, err := m.learningDesignerRepository.FindLearningDesigner(dto.GetLearningDesignerFilter{ID: &authorized.ID})
+
+				if err != nil {
+					c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": errors.New("Learning designer not found")})
+					return
+				}
 			}
-			if user.Role == role {
-				c.Set("user", user)
+
+			if authorized.Role == role || role == model.ROLE_ALL {
+				c.Set("user", authorized)
 				c.Next()
 				return
 			}
@@ -53,7 +89,7 @@ func (m *AuthMiddleware) Handler(role string) gin.HandlerFunc {
 	}
 }
 
-func (m *AuthMiddleware) authorize(tokenString string) (*uuid.UUID, error) {
+func (m *AuthMiddleware) authorize(tokenString string) (*dao.TokenClaim, error) {
 	claims := jwt.MapClaims{}
 	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
 		return []byte(m.config.JwtSecret), nil
@@ -62,13 +98,35 @@ func (m *AuthMiddleware) authorize(tokenString string) (*uuid.UUID, error) {
 	if !token.Valid || err != nil {
 		return nil, errors.New("token is invalid")
 	}
+
 	exp := claims["exp"].(float64)
+
 	if exp <= float64(time.Now().Unix()) {
 		return nil, errors.New("token expired")
 	}
 
 	id := fmt.Sprint(claims["id"])
 
-	parsedUuid, _ := uuid.Parse(id)
-	return &parsedUuid, nil
+	parsedUuid, err := uuid.Parse(id)
+
+	role := fmt.Sprint(claims["role"])
+
+	if err != nil {
+		return nil, errors.New("Cannot parse uuid")
+	}
+
+	var parsedRole model.Role
+
+	if role == model.ROLE_LEARNING_DESIGNER {
+		parsedRole = model.ROLE_LEARNING_DESIGNER
+	} else if role == model.ROLE_STUDENT {
+		parsedRole = model.ROLE_STUDENT
+	} else if role == model.ROLE_TEACHER {
+		parsedRole = model.ROLE_TEACHER
+	}
+
+	return &dao.TokenClaim{
+		ID:   parsedUuid,
+		Role: parsedRole,
+	}, nil
 }
