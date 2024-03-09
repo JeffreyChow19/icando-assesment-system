e40eb5551c235153a20711dc68e006b3510a060f
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-03-09 15:55:26 +0700

    chore: add json id

--- icando-ae-api/internal/model/dao/competency.go
+++ icando-ae-api/internal/model/dao/competency.go
@@ -3,7 +3,7 @@ package dao
 import "github.com/google/uuid"
 
 type CompetencyDao struct {
-	ID          uuid.UUID `gorm:"primarykey"`
+	ID          uuid.UUID `json:"id"`
 	Numbering   *string   `json:"numbering,omitempty"`
 	Name        *string   `json:"name,omitempty"`
 	Description *string   `json:"description,omitempty"`
