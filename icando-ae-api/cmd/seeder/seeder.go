65a1b57ed19590fbd414565c1dcd05f2736d31e0
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-03-09 18:09:25 +0700

    feat: seeder

---
+++ icando-ae-api/cmd/seeder/seeder.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+	"icando/internal/model"
+	"icando/lib"
+	"icando/utils/logger"
+	"math/rand"
+	"strings"
+	"syreclabs.com/go/faker"
+)
+
+func main() {
+	config, err := lib.NewConfig()
+	if err != nil {
+		logger.Log.Error(err.Error())
+		return
+	}
+
+	db, err := lib.NewDatabase(config)
+	if err != nil {
+		logger.Log.Error(err.Error())
+		return
+	}
+
+	tx := db.DB.Begin()
+
+	institutions := []model.Institution{
+		{ID: uuid.New(), Name: "SMAN 1 Kwangya", Nis: "23456781", Slug: "sman-1-kwangya"},
+		{ID: uuid.New(), Name: "SMAN 2 Kwangya", Nis: "23456782", Slug: "sman-2-kwangya"},
+		{ID: uuid.New(), Name: "SMAN 3 Kwangya", Nis: "23456783", Slug: "sman-3-kwangya"},
+		{ID: uuid.New(), Name: "SMAN 4 Kwangya", Nis: "23456784", Slug: "sman-4-kwangya"},
+		{ID: uuid.New(), Name: "SMAN 5 Kwangya", Nis: "23456785", Slug: "sman-5-kwangya"},
+	}
+
+	for _, institution := range institutions {
+		if err := tx.Create(&institution).Error; err != nil {
+			tx.Rollback()
+			logger.Log.Error(err.Error())
+			return
+		}
+
+		hashedPassword, _ := bcrypt.GenerateFromPassword([]byte("password"), 10)
+
+		for i := 0; i < 3; i++ {
+			firstName, lastName, email := generateAccount()
+
+			ld := model.LearningDesigner{
+				ID:            uuid.New(),
+				FirstName:     firstName,
+				LastName:      lastName,
+				Email:         email,
+				Password:      string(hashedPassword),
+				InstitutionID: institution.ID,
+			}
+
+			if err := tx.Create(&ld).Error; err != nil {
+				tx.Rollback()
+				logger.Log.Error(err.Error())
+				return
+			}
+		}
+
+		for i := 0; i < 5; i++ {
+			firstName, lastName, email := generateAccount()
+
+			teacher := model.Teacher{
+				ID:            uuid.New(),
+				FirstName:     firstName,
+				LastName:      lastName,
+				Email:         email,
+				Password:      string(hashedPassword),
+				InstitutionID: institution.ID,
+			}
+
+			if err := tx.Create(&teacher).Error; err != nil {
+				tx.Rollback()
+				logger.Log.Error(err.Error())
+				return
+			}
+
+			grades := []string{"Grade 1", "Grade 2", "Grade 3"}
+
+			for _, grade := range grades {
+				for j := 1; j <= 3; j++ {
+					class := model.Class{
+						ID:            uuid.New(),
+						Name:          fmt.Sprintf("Class %d", j),
+						Grade:         grade,
+						InstitutionID: institution.ID,
+						TeacherID:     teacher.ID,
+					}
+
+					if err := tx.Create(&class).Error; err != nil {
+						tx.Rollback()
+						logger.Log.Error(err.Error())
+						return
+					}
+
+					for k := 0; k < 8; k++ {
+						firstName, lastName, email := generateAccount()
+						nisn := fmt.Sprintf("%010d", rand.Intn(10000000000))
+
+						student := model.Student{
+							Model: model.Model{
+								ID: uuid.New(),
+							},
+							FirstName:     firstName,
+							LastName:      lastName,
+							Nisn:          nisn,
+							Email:         email,
+							InstitutionID: institution.ID,
+							ClassID:       class.ID,
+						}
+
+						if err := tx.Create(&student).Error; err != nil {
+							tx.Rollback()
+							logger.Log.Error(err.Error())
+							return
+						}
+					}
+				}
+			}
+		}
+	}
+
+	for i := 1; i <= 50; i++ {
+		competency := model.Competency{
+			Model: model.Model{
+				ID: uuid.New(),
+			},
+			Numbering:   fmt.Sprintf("C%02d", i),
+			Name:        fmt.Sprintf("Competency %d", i),
+			Description: fmt.Sprintf("Description for competency %d", i),
+		}
+
+		if err := tx.Create(&competency).Error; err != nil {
+			tx.Rollback()
+			logger.Log.Error(err.Error())
+			return
+		}
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		logger.Log.Error(err.Error())
+		return
+	}
+}
+
+func generateAccount() (firstName string, lastName string, email string) {
+	firstName = faker.Name().FirstName()
+	lastName = faker.Name().FirstName()
+	email = fmt.Sprintf("%s%s@email.com", strings.ToLower(firstName), strings.ToLower(lastName))
+
+	return firstName, lastName, email
+}
--- icando-ae-api/go.mod
+++ icando-ae-api/go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/gin-contrib/cors v1.5.0
 	github.com/gin-gonic/gin v1.9.1
+	github.com/go-playground/validator/v10 v10.15.5
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/google/uuid v1.5.0
 	github.com/joho/godotenv v1.5.1
@@ -28,7 +29,6 @@ require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.15.5 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
@@ -64,4 +64,5 @@ require (
 	golang.org/x/tools v0.16.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	syreclabs.com/go/faker v1.2.3 // indirect
 )
