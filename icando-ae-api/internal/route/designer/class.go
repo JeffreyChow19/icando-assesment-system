7c378be57b722c4ac0b3028eb075033f4d0b046e
chaerla <chelgchen@gmail.com>
2024-03-13 01:01:37 +0700

    chore: restructured auth routes

--- icando-ae-api/internal/route/designer/class.go
+++ icando-ae-api/internal/route/designer/class.go
@@ -3,30 +3,25 @@ package designer
 import (
 	"github.com/gin-gonic/gin"
 	"icando/internal/handler/designer"
-	"icando/internal/middleware"
-	"icando/internal/model/enum"
 )
 
 type ClassRoute struct {
-	classHandler   designer.ClassHandler
-	authMiddleware middleware.AuthMiddleware
+	classHandler designer.ClassHandler
 }
 
 func (r ClassRoute) Setup(group *gin.RouterGroup) {
 	group = group.Group("/class")
-	group.GET("/", r.authMiddleware.Handler(enum.ROLE_LEARNING_DESIGNER), r.classHandler.GetAll)
-	group.POST("", r.authMiddleware.Handler(enum.ROLE_LEARNING_DESIGNER), r.classHandler.Create)
-	group.GET("/:id", r.authMiddleware.Handler(enum.ROLE_LEARNING_DESIGNER), r.classHandler.Get)
-	group.PATCH("/:id", r.authMiddleware.Handler(enum.ROLE_LEARNING_DESIGNER), r.classHandler.Update)
-	group.DELETE("/:id", r.authMiddleware.Handler(enum.ROLE_LEARNING_DESIGNER), r.classHandler.Delete)
+	group.GET("/", r.classHandler.GetAll)
+	group.POST("", r.classHandler.Create)
+	group.GET("/:id", r.classHandler.Get)
+	group.PATCH("/:id", r.classHandler.Update)
+	group.DELETE("/:id", r.classHandler.Delete)
 }
 
 func NewClassRoute(
 	classHandler designer.ClassHandler,
-	authMiddleware *middleware.AuthMiddleware,
 ) *ClassRoute {
 	return &ClassRoute{
-		classHandler:   classHandler,
-		authMiddleware: *authMiddleware,
+		classHandler: classHandler,
 	}
 }
--- icando-ae-api/internal/route/student/auth.go
+++ icando-ae-api/internal/route/student/auth.go
@@ -3,19 +3,16 @@ package student
 import (
 	"github.com/gin-gonic/gin"
 	"icando/internal/handler"
-	"icando/internal/middleware"
-	"icando/internal/model/enum"
 )
 
 type AuthRoute struct {
-	authHandler    handler.AuthHandler
-	authMiddleware middleware.AuthMiddleware
+	authHandler handler.AuthHandler
 }
 
 func (r AuthRoute) Setup(engine *gin.RouterGroup) {
-	group := engine.Group("/auth")
+	group := engine.Group("/student")
 
-	group.GET("/profile", r.authMiddleware.Handler(enum.ROLE_STUDENT), r.authHandler.GetStudentProfile)
+	group.GET("/profile", r.authHandler.GetStudentProfile)
 }
 
 func NewAuthRoute(authHandler handler.AuthHandler) *AuthRoute {
