3867244eaa8ffc98262fefef0bbc525f98f0304c
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-03-13 22:02:10 +0700

    chore: change choices type to postgres.jsonb, create getter and setter for question choices.

--- icando-ae-api/internal/model/question.go
+++ icando-ae-api/internal/model/question.go
@@ -1,12 +1,33 @@
 package model
 
-import "github.com/google/uuid"
-import "encoding/json"
+import (
+	"github.com/google/uuid"
+	"github.com/jinzhu/gorm/dialects/postgres"
+	"icando/utils"
+)
 
 type Question struct {
 	Model
-	Numbering          string
-	Choices            *json.RawMessage `gorm:"type:jsonb"`
-	CorrectChoiceIndex int
-	QuizID             uuid.UUID
+	Choices  *postgres.Jsonb `gorm:"type:jsonb"`
+	AnswerID int             `gorm:"column:answer_id"`
+	QuizID   uuid.UUID       `gorm:"column:quiz_id"`
+}
+
+type QuestionChoice struct {
+	ID   int    `json:"id"`
+	Text string `json:"text"`
+}
+
+func (q *Question) GetQuestionChoices() (choices []QuestionChoice, err error) {
+	err = utils.Unmarshal(q.Choices.RawMessage, &choices)
+	return choices, err
+}
+
+func (q *Question) SetQuestionChoices(choices []QuestionChoice) error {
+	jsonChoices, err := utils.Marshal(choices)
+	if err != nil {
+		return err
+	}
+	q.Choices = &postgres.Jsonb{RawMessage: jsonChoices}
+	return nil
 }
