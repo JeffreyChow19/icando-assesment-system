be3b3f3fe5b11c630a6bbee9e46646bea10c4819
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-03-13 22:51:45 +0700

    refactor: use json marshal and unmarshal directly

--- icando-ae-api/internal/model/question.go
+++ icando-ae-api/internal/model/question.go
@@ -1,9 +1,9 @@
 package model
 
 import (
+	"encoding/json"
 	"github.com/google/uuid"
 	"github.com/jinzhu/gorm/dialects/postgres"
-	"icando/utils"
 )
 
 type Question struct {
@@ -19,12 +19,12 @@ type QuestionChoice struct {
 }
 
 func (q *Question) GetQuestionChoices() (choices []QuestionChoice, err error) {
-	err = utils.Unmarshal(q.Choices.RawMessage, &choices)
+	err = json.Unmarshal(q.Choices.RawMessage, &choices)
 	return choices, err
 }
 
 func (q *Question) SetQuestionChoices(choices []QuestionChoice) error {
-	jsonChoices, err := utils.Marshal(choices)
+	jsonChoices, err := json.Marshal(choices)
 	if err != nil {
 		return err
 	}
