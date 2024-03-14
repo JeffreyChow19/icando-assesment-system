546e536950d41a6ef11a4e0b457accf36692e464
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-03-14 22:26:26 +0700

    feat: update quiz service

--- icando-ae-api/internal/domain/service/quiz.go
+++ icando-ae-api/internal/domain/service/quiz.go
@@ -3,15 +3,18 @@ package service
 import (
 	"github.com/google/uuid"
 	"github.com/pkg/errors"
+	"gorm.io/gorm"
 	"icando/internal/domain/repository"
 	"icando/internal/model"
 	"icando/internal/model/dao"
+	"icando/internal/model/dto"
 	"icando/utils/httperror"
 	"net/http"
 )
 
 type QuizService interface {
 	CreateQuiz(id uuid.UUID) (*dao.QuizDao, *httperror.HttpError)
+	UpdateQuiz(userID uuid.UUID, quizDto dto.UpdateQuizDto) (*dao.QuizDao, *httperror.HttpError)
 }
 
 type QuizServiceImpl struct {
@@ -37,6 +40,50 @@ func (s *QuizServiceImpl) CreateQuiz(id uuid.UUID) (*dao.QuizDao, *httperror.Htt
 
 	if err != nil {
 		return nil, ErrCreateQuiz
+	}
+
+	quizDao := quiz.ToDao()
+
+	return &quizDao, nil
+}
+
+var ErrQuizNotFound = &httperror.HttpError{
+	StatusCode: http.StatusNotFound,
+	Err:        errors.New("Quiz Not Found"),
+}
+
+var ErrUpdateQuiz = &httperror.HttpError{
+	StatusCode: http.StatusInternalServerError,
+	Err:        errors.New("Unexpected error happened when updating quiz"),
+}
+
+func (s *QuizServiceImpl) UpdateQuiz(userID uuid.UUID, quizDto dto.UpdateQuizDto) (*dao.QuizDao, *httperror.HttpError) {
+	quiz, err := s.quizRepository.GetQuiz(dto.GetQuizFilter{ID: quizDto.ID})
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, ErrQuizNotFound
+		}
+		return nil, ErrUpdateQuiz
+	}
+
+	quiz.UpdatedBy = &userID
+
+	if quizDto.Name != nil {
+		quiz.Name = quizDto.Name
+	}
+	if quizDto.Subject != nil {
+		quiz.Subject = quizDto.Subject
+	}
+	if quizDto.PassingGrade != 0 {
+		quiz.PassingGrade = quizDto.PassingGrade
+	}
+	if quizDto.Deadline != nil {
+		quiz.Deadline = quizDto.Deadline
+	}
+
+	err = s.quizRepository.UpdateQuiz(*quiz)
+	if err != nil {
+		return nil, ErrUpdateQuiz
 	}
 
 	quizDao := quiz.ToDao()
