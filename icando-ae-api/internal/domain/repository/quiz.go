2360e72113ae363c0e1af6295252af65d03db957
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-03-14 22:26:13 +0700

    feat: getone and update repo

--- icando-ae-api/internal/domain/repository/quiz.go
+++ icando-ae-api/internal/domain/repository/quiz.go
@@ -1,8 +1,10 @@
 package repository
 
 import (
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 	"icando/internal/model"
+	"icando/internal/model/dto"
 	"icando/lib"
 )
 
@@ -16,7 +18,27 @@ func NewQuizRepository(db *lib.Database) QuizRepository {
 	}
 }
 
+func (r *QuizRepository) GetQuiz(filter dto.GetQuizFilter) (*model.Quiz, error) {
+	query := r.db.Model(&model.Quiz{})
+
+	if filter.ID != uuid.Nil {
+		query.Where("id = ?", filter.ID)
+	}
+
+	var quiz model.Quiz
+	err := query.First(&quiz).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &quiz, nil
+}
+
 func (r *QuizRepository) CreateQuiz(quiz model.Quiz) (model.Quiz, error) {
 	err := r.db.Create(&quiz).Error
 	return quiz, err
+}
+
+func (r *QuizRepository) UpdateQuiz(quiz model.Quiz) error {
+	return r.db.Save(&quiz).Error
 }
