4e80e32dafbf9cbb0559d04c31925335074030e9
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-03-18 15:26:41 +0700

    feat: question dao

---
+++ icando-ae-api/internal/model/dao/question.go
@@ -0,0 +1,18 @@
+package dao
+
+import (
+	"github.com/google/uuid"
+)
+
+type QuestionDao struct {
+	ID           uuid.UUID           `json:"id"`
+	Text         string              `json:"text"`
+	Choices      []QuestionChoiceDao `json:"choices"`
+	AnswerID     int                 `json:"answer_id"`
+	Competencies []CompetencyDao     `json:"competencies"`
+}
+
+type QuestionChoiceDao struct {
+	ID   int    `json:"id"`
+	Text string `json:"text"`
+}
