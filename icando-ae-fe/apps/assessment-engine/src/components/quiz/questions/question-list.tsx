3b93e6396ba97dc1397c8981fcffe87232dabdbc
chaerla <chelgchen@gmail.com>
2024-03-19 19:27:30 +0700

    feat: add collapsible details on question list

--- icando-ae-fe/apps/assessment-engine/src/components/quiz/questions/question-list.tsx
+++ icando-ae-fe/apps/assessment-engine/src/components/quiz/questions/question-list.tsx
@@ -1,27 +1,124 @@
 import {
   Table,
   TableBody,
-  TableCaption, TableCell,
+  TableCaption,
+  TableCell,
   TableHead,
   TableHeader,
-  TableRow
+  TableRow,
 } from "@ui/components/ui/table.tsx";
-import {Question} from "../../../interfaces/question.ts";
-import {useState} from "react";
-import {SearchIcon} from "lucide-react";
-import {Pagination} from "../../pagination.tsx";
-import {QuestionForm} from "./question-form.tsx";
+import { Choice, Question } from "../../../interfaces/question.ts";
+import { useState } from "react";
+import { ChevronsUpDown, GripVertical, SearchIcon } from "lucide-react";
+import { Pagination } from "../../pagination.tsx";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@ui/components/ui/collapsible.tsx";
+import { Button } from "@ui/components/ui/button.tsx";
+import { cn } from "@ui/lib/utils.ts";
+import { QuestionForm } from "./question-form.tsx";
+import { Badge } from "@ui/components/ui/badge.tsx";
 
 interface QuestionListProps {
-  questions: Question[]
+  questions: Question[];
 }
-export const QuestionList = ({questions} : QuestionListProps) => {
+
+const ChoiceList = ({
+  choices,
+  correctAnswer,
+}: {
+  choices: Choice[];
+  correctAnswer: number;
+}) => {
+  return (
+    <div className="w-full flex flex-col">
+      {choices.map((choice) => {
+        return (
+          <div
+            key={choice.id}
+            className={cn(
+              correctAnswer == choice.id && "bg-green-100",
+              "flex w-full p-2 items-center gap-6 rounded-md",
+            )}
+          >
+            <GripVertical className="size-3.5 text-muted-foreground opacity-50" />
+            <p>{choice.text}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export const QuestionList = ({ questions }: QuestionListProps) => {
   const [page, setPage] = useState<number>(1);
   const questionsPerPage = 10;
   const startIndex = (page - 1) * questionsPerPage;
   const endIndex = startIndex + questionsPerPage;
   const paginatedQuestions = questions.slice(startIndex, endIndex);
   const totalPage = Math.ceil(questions.length / questionsPerPage);
+
+  const QuestionDetails = ({
+    question,
+    index,
+  }: {
+    question: Question;
+    index: number;
+  }) => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    return (
+      <Collapsible
+        open={isOpen}
+        onOpenChange={setIsOpen}
+        className="w-full space-y-2"
+        asChild
+      >
+        <>
+          <TableRow key={question.id}>
+            <TableCell>{(page - 1) * questionsPerPage + index + 1}</TableCell>
+            <TableCell>{question.text}</TableCell>
+            <TableCell className="text-right">
+              <CollapsibleTrigger asChild>
+                <Button variant="ghost" size="sm" className="w-9 p-0">
+                  <ChevronsUpDown className="h-4 w-4" />
+                  <span className="sr-only">Toggle</span>
+                </Button>
+              </CollapsibleTrigger>
+            </TableCell>
+          </TableRow>
+          <CollapsibleContent asChild>
+            <TableRow className="bg-muted">
+              <TableCell colSpan={4}>
+                <div className="w-full flex flex-col gap-4 px-8 py-4">
+                  <h2 className="font-bold">Choices</h2>
+                  <ChoiceList
+                    choices={question.choices}
+                    correctAnswer={question.answerId}
+                  />
+                  <h2 className="font-bold">Competencies</h2>
+                  <div className="flex-wrap">
+                    {question.competencies.map((competency) => {
+                      return (
+                        <Badge key={competency.id} variant="outline">
+                          {competency.numbering} - {competency.name}
+                        </Badge>
+                      );
+                    })}
+                  </div>
+                  <div className="flex w-full justify-end">
+                    <QuestionForm type="edit" question={question} />
+                  </div>
+                </div>
+              </TableCell>
+            </TableRow>
+          </CollapsibleContent>
+        </>
+      </Collapsible>
+    );
+  };
+
   return (
     <Table>
       <TableCaption>
@@ -47,15 +144,13 @@ export const QuestionList = ({questions} : QuestionListProps) => {
       </TableHeader>
       <TableBody>
         {paginatedQuestions.map((question, index) => (
-          <TableRow key={question.id}>
-            <TableCell>{(page - 1)*questionsPerPage + index + 1}</TableCell>
-            <TableCell>{question.text}</TableCell>
-            <TableCell className="text-right">
-              <QuestionForm type="edit" />
-            </TableCell>
-          </TableRow>
+          <QuestionDetails
+            question={question}
+            index={index}
+            key={question.id}
+          />
         ))}
       </TableBody>
     </Table>
-  )
-}
+  );
+};
