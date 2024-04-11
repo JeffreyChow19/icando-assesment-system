f6bc2867c3a3991b838cd081bc03dc81ce66906f
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2024-04-11 13:26:28 +0700

    feat: generate quiz token

--- icando-ae-api/internal/model/dto/auth.go
+++ icando-ae-api/internal/model/dto/auth.go
@@ -1,8 +1,18 @@
 package dto
 
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
 type LoginDto struct {
 	Email    string `json:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
+}
+
+type GenerateQuizTokenDto struct {
+	StudentQuizId uuid.UUID
+	ExpiredAt     time.Time
 }
 
 type ChangePasswordDto struct {
