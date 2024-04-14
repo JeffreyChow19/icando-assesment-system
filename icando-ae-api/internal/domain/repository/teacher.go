d90782126929c7005252a7afc7a110ad404cfc4d
yuujin-Q <13521074@mahasiswa.itb.ac.id>
2024-04-14 13:08:55 +0700

    feat: get all teacher handler

--- icando-ae-api/internal/domain/repository/teacher.go
+++ icando-ae-api/internal/domain/repository/teacher.go
@@ -18,6 +18,27 @@ func NewTeacherRepository(db *lib.Database) TeacherRepository {
 	}
 }
 
+func (r *TeacherRepository) GetAllTeacher(filter dto.GetTeacherFilter) ([]model.Teacher, error) {
+	query := r.db.Table("teachers")
+	result := make([]model.Teacher, 0)
+
+	if filter.ID != nil {
+		query.Where("id = ?", filter.ID.String())
+	}
+	if filter.Email != nil {
+		query.Where("email = ?", *filter.Email)
+	}
+	if filter.InstitutionID != nil {
+		query.Where("institution_id = ?", filter.InstitutionID.String())
+	}
+
+	if err := query.Find(&result).Error; err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
 func (r *TeacherRepository) GetTeacher(filter dto.GetTeacherFilter) (*model.Teacher, error) {
 	var user model.Teacher
 
