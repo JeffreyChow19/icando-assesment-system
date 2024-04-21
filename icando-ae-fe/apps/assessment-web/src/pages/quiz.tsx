19e31228a99818de9cb997ab9f532e59b99ca9b1
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-04-21 18:20:20 +0700

    feat: submit quiz

--- icando-ae-fe/apps/assessment-web/src/pages/quiz.tsx
+++ icando-ae-fe/apps/assessment-web/src/pages/quiz.tsx
@@ -1,30 +1,31 @@
-import { Layout } from '../layouts/layout.tsx';
-import { useStudentQuiz } from '../context/user-context.tsx';
-import { Card, CardContent } from '@ui/components/ui/card.tsx';
-import { useMutation, useQuery } from '@tanstack/react-query';
-import { getQuizDetail, updateAnswer } from '../services/quiz.ts';
-import { useEffect, useState } from 'react';
-import { onErrorToast } from '../components/error-toast.tsx';
-import { useNavigate, useParams } from 'react-router-dom';
-import { Button } from '@ui/components/ui/button.tsx';
-import Countdown from 'react-countdown';
+import { Layout } from "../layouts/layout.tsx";
+import { useStudentQuiz } from "../context/user-context.tsx";
+import { Card, CardContent } from "@ui/components/ui/card.tsx";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { getQuizDetail, updateAnswer, submitQuiz } from "../services/quiz.ts";
+import { useEffect, useState } from "react";
+import { onErrorToast } from "../components/error-toast.tsx";
+import { useNavigate, useParams } from "react-router-dom";
+import { Button } from "@ui/components/ui/button.tsx";
+import Countdown from "react-countdown";
+import { toast } from "@ui/components/ui/use-toast.ts";
 
 export const Quiz = () => {
   const { number } = useParams();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const { studentQuiz, setStudentQuiz } = useStudentQuiz();
-  const [time, setTime] = useState<Date>()
+  const [time, setTime] = useState<Date>();
 
   const { data, isLoading, error, refetch } = useQuery({
-    queryKey: ['studentQuiz'],
+    queryKey: ["studentQuiz"],
     queryFn: () => getQuizDetail(),
-    retry: false
+    retry: false,
   });
 
   useEffect(() => {
     if (data) {
-      setTime(getRemainingTime(data.startedAt, data.quiz!.duration))
+      setTime(getRemainingTime(data.startedAt, data.quiz!.duration));
     }
   }, [data]);
 
@@ -41,59 +42,118 @@ export const Quiz = () => {
 
   const getRemainingTime = (startAt: string, duration: number) => {
     const startTime = new Date(startAt);
-    return new Date(startTime.getTime() + duration * 60000)
-  }
+    return new Date(startTime.getTime() + duration * 60000);
+  };
 
   const mutation = useMutation({
     mutationFn: async (choiceId: number) => {
-      return await updateAnswer(studentQuiz!.quiz!.questions![parseInt(number!) - 1].id, choiceId);
+      return await updateAnswer(
+        studentQuiz!.quiz!.questions![parseInt(number!) - 1].id,
+        choiceId,
+      );
     },
     onSuccess: (response) => {
       console.log(response);
-      refetch()
+      refetch();
     },
     onError: (err: Error) => {
       onErrorToast(err);
     },
   });
 
-  const renderer = ({ hours, minutes, seconds, completed } : {hours: number, minutes: number, seconds: number, completed: boolean}) => {
+  const renderer = ({
+    hours,
+    minutes,
+    seconds,
+    completed,
+  }: {
+    hours: number;
+    minutes: number;
+    seconds: number;
+    completed: boolean;
+  }) => {
     if (completed) {
-      console.log('time ended');
+      console.log("time ended");
       // TODO: submit otomatis
     } else {
-      return <span className="font-bold text-primary">{hours}:{minutes}:{seconds}</span>;
+      return (
+        <span className="font-bold text-primary">
+          {hours}:{minutes}:{seconds}
+        </span>
+      );
+    }
+  };
+
+  const submit = async () => {
+    try {
+      await submitQuiz();
+      toast({
+        description: "Quiz submitted!",
+      });
+      navigate("/");
+    } catch (err) {
+      toast({
+        variant: "destructive",
+        description: "Failed to submit quiz! Please try again!",
+      });
     }
   };
 
   return (
     <Layout pageTitle="Quiz" showTitle={true} showNavigation={true}>
-      {studentQuiz && studentQuiz.quiz && studentQuiz.quiz.questions && number && <div className="flex flex-col flex-grow justify-between">
-        <div className="flex flex-col gap-3">
-          <div className="w-full flex justify-end">
-          <Countdown
-            date={time}
-            renderer={renderer}
-          />
+      {studentQuiz &&
+        studentQuiz.quiz &&
+        studentQuiz.quiz.questions &&
+        number && (
+          <div className="flex flex-col flex-grow justify-between">
+            <div className="flex flex-col gap-3">
+              <div className="w-full flex justify-end">
+                <Countdown date={time} renderer={renderer} />
+              </div>
+              <Card className="mb-2">
+                <CardContent className="p-4">
+                  <p>{studentQuiz.quiz.questions[parseInt(number) - 1].text}</p>
+                </CardContent>
+              </Card>
+              {studentQuiz.quiz.questions[parseInt(number) - 1].choices.map(
+                (choice) => (
+                  <Button
+                    className="w-full shadow-md py-3 rounded-lg bg-background text-foreground hover:bg-blue"
+                    onClick={() => mutation.mutate(choice.id)}
+                  >
+                    {choice.text}
+                  </Button>
+                ),
+              )}
+            </div>
+            <div className="flex gap-3">
+              {parseInt(number) > 1 && (
+                <Button
+                  className="text-primary bg-background hover:bg-background/80 rounded-full w-full"
+                  onClick={() => navigate(`/quiz/${parseInt(number) - 1}`)}
+                >
+                  Kembali
+                </Button>
+              )}
+              {parseInt(number) !== studentQuiz.quiz.questions.length && (
+                <Button
+                  className="rounded-full w-full"
+                  onClick={() => navigate(`/quiz/${parseInt(number) + 1}`)}
+                >
+                  Lanjut
+                </Button>
+              )}
+              {parseInt(number) === studentQuiz.quiz.questions.length && (
+                <Button
+                  className="rounded-full w-full"
+                  onClick={() => submit()}
+                >
+                  Submit
+                </Button>
+              )}
+            </div>
           </div>
-          <Card className="mb-2">
-            <CardContent className="p-4">
-              <p>
-                {studentQuiz.quiz.questions[parseInt(number) - 1].text}
-              </p>
-            </CardContent>
-          </Card>
-          {studentQuiz.quiz.questions[parseInt(number) - 1].choices.map((choice) => <Button
-                                                                                            className="w-full shadow-md py-3 rounded-lg bg-background text-foreground hover:bg-blue" onClick={() => mutation.mutate(choice.id)}>
-            {choice.text}
-          </Button>)}
-        </div>
-        <div className="flex gap-3">
-          {parseInt(number) > 1 && <Button className="text-primary bg-background hover:bg-background/80 rounded-full w-full" onClick={() => navigate(`/quiz/${parseInt(number) - 1}`)}>Kembali</Button>}
-          {parseInt(number) !== studentQuiz.quiz.questions.length && <Button className="rounded-full w-full" onClick={() => navigate(`/quiz/${parseInt(number) + 1}`)}>Lanjut</Button>}
-          {parseInt(number) === studentQuiz.quiz.questions.length && <Button className="rounded-full w-full">Submit</Button>}
-        </div>
-      </div>}
+        )}
     </Layout>
   );
 };
