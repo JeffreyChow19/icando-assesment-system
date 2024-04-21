b9e80843ef77ebed05dfecc0d7503b684603dfe8
Alexander Jason <alexanderjason526@gmail.com>
2024-04-21 15:33:08 +0700

    fix: get quiz api with class

--- icando-ae-api/internal/domain/service/quiz.go
+++ icando-ae-api/internal/domain/service/quiz.go
@@ -91,7 +91,7 @@ var ErrUpdateQuiz = &httperror.HttpError{
 }
 
 func (s *QuizServiceImpl) GetQuiz(id uuid.UUID) (*dao.QuizDao, *httperror.HttpError) {
-	quiz, err := s.quizRepository.GetQuiz(dto.GetQuizFilter{ID: id, WithCreator: true, WithUpdater: true, WithQuestions: true})
+	quiz, err := s.quizRepository.GetQuiz(dto.GetQuizFilter{ID: id, WithCreator: true, WithUpdater: true, WithQuestions: true, WithClasses: true})
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, ErrQuizNotFound
