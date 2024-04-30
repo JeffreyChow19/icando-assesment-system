1179f1d5689dcbb5a23c6b246691bebf79cfea77
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-04-30 20:12:31 +0700

    chore: change copy for submitting quiz

--- icando-ae-fe/apps/assessment-web/src/pages/quiz.tsx
+++ icando-ae-fe/apps/assessment-web/src/pages/quiz.tsx
@@ -216,7 +216,7 @@ export const Quiz = () => {
                     })
                   }
                 >
-                  Submit
+                  Kirim
                 </Button>
               )}
             </div>
