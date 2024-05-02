a4fbb760c8a22eca47b73b74245de90579beefab
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-05-02 23:23:34 +0700

    feat: get latest submission dao

--- icando-ae-api/internal/model/dao/analytics.go
+++ icando-ae-api/internal/model/dao/analytics.go
@@ -1,6 +1,17 @@
 package dao
 
+import "time"
+
 type QuizPerformanceDao struct {
 	QuizzesPassed int `json:"quizzesPassed"`
 	QuizzesFailed int `json:"quizzesFailed"`
+}
+
+type GetLatestSubmissionsDao struct {
+	ClassName   string    `json:"class_name"`
+	Grade       string    `json:"grade"`
+	QuizName    string    `json:"quiz_name"`
+	FirstName   string    `json:"first_name"`
+	LastName    string    `json:"last_name"`
+	CompletedAt time.Time `json:"completed_at"`
 }
