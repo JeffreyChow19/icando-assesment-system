b2409da33f8500562f12c791f5ab4f6eda17275e
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-05-03 22:36:35 +0700

    feat(ad-01-11): student info component

---
+++ icando-ae-fe/apps/analytics-dashboard/src/components/student/student-info.tsx
@@ -0,0 +1,29 @@
+import { StudentData } from "../../interfaces/student.ts";
+
+interface StudentInfoProps {
+  data: StudentData;
+}
+
+export const StudentInfo = ({ data }: StudentInfoProps) => {
+  return (
+    <div className="grid grid-cols-3 gap-y-2 gap-x-4 max-w-[400px] bg-gray-100 p-2 divide-y">
+      <div className="text-right font-semibold">NISN</div>
+      <div className="col-span-2">{data.student.nisn}</div>
+
+      <div className="text-right font-semibold">Name</div>
+      <div className="col-span-2">
+        {data.student
+          ? `${data.student.firstName} ${data.student.lastName}`
+          : ""}
+      </div>
+
+      <div className="text-right font-semibold">Email</div>
+      <div className="col-span-2">{data.student.email}</div>
+
+      <div className="text-right font-semibold">Class</div>
+      <div className="col-span-2">
+        {data.student ? `${data.class.grade} - ${data.class.name}` : ""}
+      </div>
+    </div>
+  );
+};
