ace260f3e5f39aec846f693b5ce8763305e50647
chaerla <chelgchen@gmail.com>
2024-05-03 21:32:40 +0700

    feat: add format date on ad fe

---
+++ icando-ae-fe/apps/analytics-dashboard/src/utils/format-date.ts
@@ -0,0 +1,24 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+export function formatDate(date: Date): string {
+  return dayjs(date)
+    .tz("Asia/Jakarta")
+    .locale("id")
+    .format("dddd, D MMMM YYYY");
+}
+
+export function formatDateHour(date: Date): string {
+  return dayjs(date)
+    .tz("Asia/Jakarta")
+    .locale("id")
+    .format("ddd, DD MMMM YYYY HH:mm [WIB]");
+}
+
+export function formatHour(date: Date): string {
+  return dayjs(date).tz("Asia/Jakarta").locale("id").format("HH:mm") + " WIB";
+}
