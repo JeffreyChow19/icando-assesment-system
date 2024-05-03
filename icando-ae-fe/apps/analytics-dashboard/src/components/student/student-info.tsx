7e556f9d3500cf4739267548c8ab7ece4556565e
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-05-03 23:59:48 +0700

    chore(ad-01-11): text left

--- icando-ae-fe/apps/analytics-dashboard/src/components/student/student-info.tsx
+++ icando-ae-fe/apps/analytics-dashboard/src/components/student/student-info.tsx
@@ -6,21 +6,21 @@ interface StudentInfoProps {
 
 export const StudentInfo = ({ data }: StudentInfoProps) => {
   return (
-    <div className="grid grid-cols-3 gap-y-2 gap-x-4 max-w-[400px] p-2 divide-y">
-      <div className="text-right font-semibold">NISN</div>
+    <div className="grid grid-cols-3 gap-y-2 gap-x-4 max-w-[400px] p-2 ">
+      <div className="text-left font-semibold">NISN</div>
       <div className="col-span-2">{data.student.nisn}</div>
 
-      <div className="text-right font-semibold">Name</div>
+      <div className="text-left font-semibold">Name</div>
       <div className="col-span-2">
         {data.student
           ? `${data.student.firstName} ${data.student.lastName}`
           : ""}
       </div>
 
-      <div className="text-right font-semibold">Email</div>
+      <div className="text-left font-semibold">Email</div>
       <div className="col-span-2">{data.student.email}</div>
 
-      <div className="text-right font-semibold">Class</div>
+      <div className="text-left font-semibold">Class</div>
       <div className="col-span-2">
         {data.student ? `${data.class.grade} - ${data.class.name}` : ""}
       </div>
