b6e5faa8ea12cf2e6b16a45365e9327218c92a54
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-05-03 22:34:34 +0700

    chore(ad-01-11): adjust dao

--- icando-ae-api/internal/model/dao/analytics.go
+++ icando-ae-api/internal/model/dao/analytics.go
@@ -1,6 +1,9 @@
 package dao
 
-import "time"
+import (
+	"github.com/google/uuid"
+	"time"
+)
 
 type QuizPerformanceDao struct {
 	QuizzesPassed int `json:"quizzesPassed"`
@@ -17,13 +20,15 @@ type GetLatestSubmissionsDao struct {
 }
 
 type GetStudentQuizCompetencyDao struct {
-	Numbering  string `json:"numbering"`
-	Name       string `json:"name"`
-	CorrectSum int    `json:"correct_sum"`
-	TotalSum   int    `json:"total_sum"`
+	Numbering  string `json:"competencyId"`
+	Name       string `json:"competencyName"`
+	CorrectSum int    `json:"correctCount"`
+	TotalSum   int    `json:"totalCount"`
 }
 
 type GetStudentQuizzesDao struct {
+	ID           uuid.UUID `json:"id"`
+	QuizID       uuid.UUID `json:"quiz_id"`
 	TotalScore   float32   `json:"total_score"`
 	CorrectCount int       `json:"correct_count"`
 	CompletedAt  time.Time `json:"completed_at"`
@@ -31,8 +36,13 @@ type GetStudentQuizzesDao struct {
 	PassingGrade float64   `json:"passing_grade"`
 }
 
+type StudentInfo struct {
+	Student StudentDao `json:"student"`
+	Class   ClassDao   `json:"class"`
+}
+
 type GetStudentStatisticsDao struct {
-	Student     StudentDao                    `json:"student"`
+	StudentInfo StudentInfo                   `json:"student_info"`
 	Performance QuizPerformanceDao            `json:"performance"`
 	Competency  []GetStudentQuizCompetencyDao `json:"competency"`
 	Quizzes     []GetStudentQuizzesDao        `json:"quizzes"`
